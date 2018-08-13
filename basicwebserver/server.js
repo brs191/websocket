17461efecea1d1b7422638defb2c3973f88b758a
brs191 <rajashekar.b@gmail.com>
2018-08-13 23:45:27 +0900

    Basic WebSocket Server

---
+++ basicwebserver/server.js
@@ -0,0 +1,9 @@
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({port: 8181});
+
+wss.on('connection', function (ws) {
+    console.log('client connected');
+    ws.on('message', function(message) {
+        console.log(message);
+    });
+});
